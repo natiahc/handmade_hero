1a1defbdddf8c9d44f95d49ad35529d1ce5367a3
Chaitan Divagi <natiahc@gmail.com>
2020-01-13 01:56:33 +0530

    Opening a Win32 Window
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,12 +1,124 @@
 #include <windows.h>
 
-int CALLBACK
-WinMain(HINSTANCE hInstance,
-		HINSTANCE hPrevInstance,
-		LPSTR lpCmdLine,
-		int nCmdShow)
+LRESULT CALLBACK MainWindowCallback(
+	HWND   Window,
+	UINT   Message,
+	WPARAM WParam,
+	LPARAM LParam)
 {
-	//GetModuleHandle(0);
-	MessageBox(0, "Test Text", "Test Title", MB_OK|MB_ICONINFORMATION);
+	LRESULT Result = 0;
+
+	switch(Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+		} break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+		} break;
+
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WM_CLOSE\n");
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+
+		case WM_PAINT:
+		{
+			
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			static DWORD Operation = WHITENESS;
+			PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+			if(Operation == WHITENESS)
+			{
+				Operation = BLACKNESS;
+			}
+			else 
+			{
+				Operation = WHITENESS;
+			}
+			
+			EndPaint(Window, &Paint);
+		} break;
+
+		default:
+		{
+			//OutputDebugStringA("default\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return(Result);
+}
+
+int CALLBACK
+WinMain(HINSTANCE Instance,
+		HINSTANCE PrevInstance,
+		LPSTR 	  CommandLine,
+		int 	  ShowCode)
+{
+	WNDCLASS WindowClass = {};
+	
+	WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+	if(RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle = 
+		CreateWindowEx( 
+			0, 
+			WindowClass.lpszClassName,
+			"Handmade_Hero",
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE, 
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			Instance,
+  			0);
+
+		if(WindowHandle)
+		{
+			for(;;)
+			{
+				MSG Message;
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if(MessageResult > 0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+
+		}
+	}
+	else
+	{
+		// TODO(chaitan): Logging
+	}
+
 	return(0);
 }
